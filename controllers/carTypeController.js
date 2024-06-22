3062d281dd7f93090f4c313bf5f1cd95a4209675
Riyan-Mo <riyan8308151899@gmail.com>
2024-06-22 17:40:43 +0530

    Added creating and updating routes for car types

--- controllers/carTypeController.js
+++ controllers/carTypeController.js
@@ -2,6 +2,7 @@ const asyncHandler = require("express-async-handler");
 const CarType = require("../models/CarType")
 const Car = require("../models/Car");
 const Company = require("../models/Company")
+const {body, validationResult} = require("express-validator");
 
 exports.index = asyncHandler(async(req, res, next)=>{
     const carTypes = await CarType.find({}).exec();
@@ -32,3 +33,70 @@ exports.postDelete = asyncHandler(async(req, res, next)=>{
     }
     res.redirect(`/types/${id}`);
 })
+
+exports.getCreate = (req, res, next)=>{
+    res.render("carTypeForm");
+}
+
+exports.postCreate = [
+    body("name", "Type name is not valid.")
+    .exists()
+    .isString({min:3})
+    .escape(),
+
+    body("photoUrl", "Photo Url is not valid.")
+    .exists()
+    .isString({min: 10}),
+
+    body("description", "Description is not valid.")
+    .exists()
+    .isString({min: 10})
+    .escape(),
+
+    asyncHandler(async(req, res, next)=>{
+        const errors = validationResult(req);
+        const typeDetails = req.body;
+        if(errors.isEmpty()){
+            const type = await new CarType(typeDetails).save();
+            res.redirect(type.url);
+        }
+        else{
+            res.render("carTypeForm", {type: typeDetails, errors: errors.errors.map(e=>e.msg)});
+        }
+    })
+]
+
+exports.getUpdate = asyncHandler(async(req, res, next)=>{
+    const id = req.params.id;
+    const type = await CarType.findById(id).exec();
+    res.render("carTypeForm", {type});
+})
+
+exports.postUpdate = [
+    body("name", "Type name is not valid.")
+    .exists()
+    .isString({min:3})
+    .escape(),
+
+    body("photoUrl", "Photo Url is not valid.")
+    .exists()
+    .isString({min: 10}),
+
+    body("description", "Description is not valid.")
+    .exists()
+    .isString({min: 10})
+    .escape(),
+
+    asyncHandler(async(req, res, next)=>{
+        const errors = validationResult(req);
+        const typeDetails = req.body;
+        if(errors.isEmpty()){
+            const id = req.params.id;
+            const updatedType = await CarType.findByIdAndUpdate(id, typeDetails).exec();
+            res.redirect(updatedType.url);
+        }
+        else{
+            res.render("carTypeForm", {type: typeDetails, errors: errors.errors.map(e=>e.msg)});
+        }
+    })
+]
--- routes/cartypes.js
+++ routes/cartypes.js
@@ -3,10 +3,18 @@ const carTypeController = require("../controllers/carTypeController");
 
 router.get("/", carTypeController.index);
 
+router.get("/create", carTypeController.getCreate);
+
 router.get("/:id", carTypeController.id);
 
 router.get("/:id/delete", carTypeController.getDelete);
 
+router.get("/:id/update", carTypeController.getUpdate);
+
+router.post("/create", carTypeController.postCreate);
+
 router.post("/:id/delete", carTypeController.postDelete);
+
+router.post("/:id/update", carTypeController.postUpdate);
 
 module.exports = router;
