d5b758a5ed15f7f4ce33b41f9f27682bd8922df8
Riyan-Mo <riyan8308151899@gmail.com>
2024-06-22 18:18:01 +0530

    Added compression to compress routes

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 require("dotenv/config")
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const compression = require("compression");
 
 var indexRouter = require('./routes/index');
 const carsRouter = require('./routes/cars');
@@ -40,6 +42,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(compression())
 
 app.use('/', indexRouter);
 app.use('/cars', carsRouter);
